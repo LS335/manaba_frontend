504757812e2d8cf3f3535a2de3df92eb1d71883a
Ohta <OhtaPano@vivaldi.net>
2021-11-17 01:44:34 +0900

    BoardDisplay 作成

--- src/features/board/BoardDisplay.tsx
+++ src/features/board/BoardDisplay.tsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectSelectedBoard } from './boardSlice';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableRow,
+} from '@material-ui/core';
 
 const BoardDisplay: React.FC = () => {
+    const selectedBoard = useSelector(selectSelectedBoard);
+    const rows = [
+        { item: "Title", data: selectedBoard.title  },
+        { item: "Board", data: selectedBoard.board },
+        { item: "Category", data: selectedBoard.category_item },
+        { item: "Created", data: selectedBoard.created_at },
+        { item: "Updated", data: selectedBoard.updated_at },
+    ];
+
+    if (!selectedBoard.board) {
+        return null;
+    };
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <h2>詳細</h2>
+            <Table>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.item}>
+                            <TableCell align="center">
+                                <strong>{row.item}</strong>
+                            </TableCell>
+                            <TableCell align="center">{row.data}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </>
     )
 }
 
-export default BoardDisplay
+export default BoardDisplay;
